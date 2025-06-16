53a6f13190b5ee79e515ec323cf911180dab40bf
ScorpioGK <scorpiogameking@gmail.com>
2025-06-16 12:35:16 -0400

    began parser package

--- main.go
+++ main.go
@@ -4,13 +4,9 @@ import (
     "bytes"
     "compress/zlib"
     //"encoding/base64"
-    "fmt"
 	"flag"
     "bufio"
     "os"
-	"strings"
-	"io/ioutil"
-	"encoding/json"
 	"github.com/ScorpioGameKing/krokifier/parser"
 )
 
@@ -49,54 +45,6 @@ func Encode(input string, buf *bytes.Buffer) (int, error) {
     return n, nil
 }
 
-type SyntaxFile struct {
-	UML_Type string
-	Language string
-	Diagram []UMLGroups
-}
-
-type UMLGroups struct {
-	Label string
-	Keywords []Keyword
-}
-
-type Keyword struct {
-	Word string
-	Extension string
-	Extends bool 
-	Recursive bool
-	Inherits bool
-}
-
-type UMLTypes struct {
-	Connecting string
-}
-
-func LoadSyntaxFile(path string, syntax *SyntaxFile) (error) {
-    file, err := ioutil.ReadFile(path)
-
-    if err != nil {
-        return err
-    }
-
-    return json.Unmarshal(file, syntax)
-}
-
-func LoadUMLTypes(uml_type string, umltypes *UMLTypes) (error) {
-	var path string
-	switch uml_type {
-		case "blockdiag":
-			path = "./res/language/blockdiag.json"
-	}
-	file, err := ioutil.ReadFile(path)
-
-	if err != nil {
-		return err
-	}
-
-	return json.Unmarshal(file, umltypes)
-}
-
 func CheckErr(err error) {
 	if err != nil {
 		panic(err)
@@ -105,11 +53,9 @@ func CheckErr(err error) {
 
 func main() {
 	var err error
-    var buf bytes.Buffer
-	var syntax SyntaxFile 
-	var umltypes UMLTypes
+	var syntax parser.SyntaxFile 
+	var umltypes parser.UMLTypes
     var lines []string
-	var output []string
 
     file_path := flag.String("path", "", "The file to parse")
 	syntax_path := flag.String("syntax", "", "The syntax file used to generate UML")
@@ -120,85 +66,14 @@ func main() {
 
 	CheckErr(err)
 
-	err = LoadSyntaxFile(*syntax_path, &syntax)
+	err = parser.LoadSyntaxFile(*syntax_path, &syntax)
 
 	CheckErr(err)
 
-	err = LoadUMLTypes(syntax.UML_Type, &umltypes)
+	err = parser.LoadUMLTypes(syntax.UML_Type, &umltypes)
 
 	CheckErr(err)
 
-	fmt.Printf("\nPrinting Syntax File Diagram\n")
-	for i, group := range syntax.Diagram {
-		fmt.Printf("%d : %+v\n", i, group)
-	}
+	_, err = parser.ParseFile(lines, &syntax, &umltypes)
 
-	fmt.Printf("\nParsing Input File\n")
-	for _, line := range lines {
-		_, err := Encode(line, &buf)
-
-		CheckErr(err)
-
-    	//result := base64.URLEncoding.EncodeToString(buf.Bytes())
-
-		words := strings.Split(strings.TrimLeft(line, " "), " ")
-		for _, group := range syntax.Diagram {
-			var parsed_string bytes.Buffer
-			var recursive_string []string
-			for _, kword := range group.Keywords {
-				switch words[0] {
-					case kword.Word:
-						if !kword.Recursive {
-							if kword.Extension == "" {
-								parsed_string.WriteString(words[1])
-							} else {
-								parsed_string.WriteString(strings.ReplaceAll(words[1], kword.Extension, umltypes.Connecting))		
-							}	
-						} else {
-							recursive_string = strings.Split(words[1], kword.Extension)
-							for _, recursive_word := range recursive_string {
-								parsed_string.WriteString(recursive_word)
-							}
-						}
-
-						fmt.Printf("\nFOUND KEYWORD: %s FROM GROUP: %+v IN LINE: %v WILL DISPLAY AS: %s\n", kword.Word, group, words, parsed_string.String())
-
-						if len(group.Keywords) > 1 {
-							sub_words := words[2:]
-							for i, sub_word := range sub_words {
-								for _, sub_kword := range group.Keywords[1:] {
-									switch sub_word {
-										case sub_kword.Word:
-											if !sub_kword.Recursive {
-												if sub_kword.Extension == "" {
-													parsed_string.WriteString(fmt.Sprintf("%s%s", umltypes.Connecting, sub_words[i+1]))
-												} else {
-													parsed_string.WriteString(fmt.Sprintf("%s%s", umltypes.Connecting, strings.ReplaceAll(sub_words[i+1], sub_kword.Extension, umltypes.Connecting)))		
-												}	
-											} else {
-												current_string := parsed_string
-												for n, recursive_word := range sub_words[i+1:] {
-													if n >= 1 {
-														parsed_string.WriteString(fmt.Sprintf("%s%s%s\n", current_string.String(), umltypes.Connecting, recursive_word))
-													} else {
-														parsed_string.WriteString(fmt.Sprintf("%s%s\n", umltypes.Connecting, recursive_word))
-													}
-												}
-
-											}
-											fmt.Printf("\nFOUND KEYWORD: %s FROM GROUP: %+v IN LINE: %v WILL DISPLAY AS: %s\n", sub_kword.Word, group, sub_words, sub_words[i+1])
-									}
-							}
-						}
-					}
-					output = append(output, "group {\n")
-					output = append(output, "}\n")
-				}
-			}
-		}
-    	//fmt.Printf("%d : %s\n", i, line)
-    }
-
-	fmt.Printf("OUTPUT: %v", output)
-	parser.test("Hello Package!")
 }
--- parser/parser.go
+++ parser/parser.go
@@ -2,8 +2,133 @@ package parser
 
 import (
 	"fmt"
+	"strings"
+	"io/ioutil"
+	"encoding/json"
 )
 
-func test(msg string) {
-	fmt.Printf("Your Message: %s", msg)
+// Used for loading the user given settings from JSON
+
+type SyntaxFile struct {
+	UML_Type string
+	Language string
+	Diagram []UMLGroups
+}
+
+type UMLGroups struct {
+	Label string
+	Keywords []Keyword
+}
+
+type Keyword struct {
+	Word string
+	Extension string
+	Extends bool 
+	Recursive bool
+	Inherits bool
+}
+
+type UMLTypes struct {
+	Connecting string
+}
+
+// ------------------------------
+
+// Used by krokifier to properly build output
+
+type ParsedGroup struct {
+	Label string
+	RawWords map[int][]string
+	LeadingKey Keyword
+	SubKeys []Keyword
+}
+
+type FileGroup struct {
+	Groups map[string]ParsedGroup
+}
+
+// ------------------------------
+
+// JSON File Loading
+
+func LoadSyntaxFile(path string, syntax *SyntaxFile) (error) {
+    file, err := ioutil.ReadFile(path)
+
+    if err != nil {
+        return err
+    }
+
+    return json.Unmarshal(file, syntax)
+}
+
+func LoadUMLTypes(uml_type string, umltypes *UMLTypes) (error) {
+	var path string
+	switch uml_type {
+		case "blockdiag":
+			path = "./res/language/blockdiag.json"
+	}
+	file, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(file, umltypes)
+}
+
+// ------------------------------
+
+func ParseFile(lines []string, syntax *SyntaxFile, umltypes *UMLTypes) (FileGroup, error) {
+	var file FileGroup
+	file.Groups = map[string]ParsedGroup{}
+
+	fmt.Printf("\nPrinting Syntax File Diagram\n")
+
+	for i, group := range syntax.Diagram {
+		fmt.Printf("%d : %+v\n", i, group)
+	}
+
+	fmt.Printf("\nParsing Input File\n")
+
+	for _, line := range lines {
+		//fmt.Printf("%d : %s\n", i, line)
+		
+		words := strings.Split(strings.TrimLeft(line, " "), " ")
+		
+		for _, group := range syntax.Diagram {
+			for _, kword := range group.Keywords {
+				switch words[0] {
+					case kword.Word:
+						fmt.Printf("Found Group: %s\n", group.Label)
+						
+						if file_group, ok := file.Groups[group.Label]; ok {
+							file_group.RawWords[len(file_group.RawWords)] = words
+							fmt.Printf("%+v\n", file_group)
+						
+						} else {
+							var new_group ParsedGroup
+							var raw_words map[int][]string
+
+							raw_words = map[int][]string{}
+							raw_words[0] = words
+
+							new_group.Label = group.Label
+							new_group.RawWords = raw_words
+							new_group.LeadingKey = kword
+							
+							if len(group.Keywords) > 1 {
+								new_group.SubKeys = group.Keywords[1:]
+							} else {
+								var no_keys []Keyword
+								new_group.SubKeys = no_keys
+							}
+							
+							file.Groups[group.Label] = new_group
+						}
+				}
+			}
+		}
+	}
+
+	return file, nil
 }
